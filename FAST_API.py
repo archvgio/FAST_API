d180ecc0816b1e6d506316606398ad8e9ded9b60
Gio <gioacompanado.pro@gmail.com>
2026-05-24 15:56:59 +0800

    Rename parameter in say_hello function

--- FAST_API.py
+++ FAST_API.py
@@ -8,6 +8,6 @@ def read_root():
     return {"message": "Hello API"}
 
 
-@app.get("/hello/{Gio}")
-def say_hello(Gio: str):
-    return {"message": f"Hello, {Gio}!"}
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello, {name}!"}
